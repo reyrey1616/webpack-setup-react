4b41d0e57a5041695fff1e993a6924ccb83e2713
yobbad16 <dabboyrey@gmail.com>
2020-05-22 00:42:09 +0800

    Added the loaders and presets

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,17 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
